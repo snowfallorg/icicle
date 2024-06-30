e5d532a13783a61fc432a39245f35e3485f6b124
Jan Tojnar <jtojnar@gmail.com>
2024-06-30 21:27:53 +0200

    partitions: Use ≥ for min partition size
    Otherwise, we will be unable to install it in GNOME Boxes with the default NixOS hardware preset of 20 GiB.

--- src/ui/pages/partitions.rs
+++ src/ui/pages/partitions.rs
@@ -549,9 +549,9 @@ impl FactoryComponent for WholeDisk {
             #[watch]
             // Translators: Do NOT translate the '{}'
             // The string reads "{/dev/sdX} (20 GB minimum needed)" indicating that the given disk is not large enough
-            set_subtitle: &if self.size > 21_474_836_480  { size::Size::from_bytes(self.size).to_string() } else { i18n_f("{} (20 GB minimum needed)", &[&size::Size::from_bytes(self.size).to_string()]) },
+            set_subtitle: &if self.size >= 21_474_836_480  { size::Size::from_bytes(self.size).to_string() } else { i18n_f("{} (20 GB minimum needed)", &[&size::Size::from_bytes(self.size).to_string()]) },
             set_activatable: true,
-            set_sensitive: self.size > 21_474_836_480, // 20GB
+            set_sensitive: self.size >= 21_474_836_480, // 20GB
             #[name(checkbtn)]
             add_suffix = &gtk::CheckButton {
                 set_group: Some(&self.group),
